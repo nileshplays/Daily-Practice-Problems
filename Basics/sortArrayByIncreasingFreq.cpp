96aa6bfd1520649c4a86c51f22f5c65f70ae4ce6
nileshplays <nileshsarkar7731@gmail.com>
2024-07-23 23:57:23 +0530

    Sort Array By Increasing Frequency

---
+++ Basics/sortArrayByIncreasingFreq.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for (auto i : nums) {
+            map[i]++;
+        }
+        sort(nums.begin(), nums.end(), [&map](int a, int b) {
+            if (map[a] == map[b]) {
+                return a > b;
+            }
+            return map[a] < map[b];
+        });
+        return nums;
+    }
+};
