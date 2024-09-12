7a145649973acd91efecd380730f3bf440dce495
nileshplays <nileshsarkar7731@gmail.com>
2024-09-13 03:05:15 +0530

    Index Of First occurance Of String

---
+++ String/indexOfFirstOccOfString.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        for(int i=0; i<haystack.size(); i++){
+            if(haystack.substr(i,needle.size()) == needle){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+};
